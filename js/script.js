a83c2a2faedbcb1b399df2867ad1edd89e6f2f16
lucagad <lucagaddini@icloud.com>
2022-05-06 12:01:17 +0200

    {HTML/CSS} Added: .search_bar and .send_message_bar with relative styles - Fixed: .contacts_box

--- js/script.js
+++ js/script.js
@@ -173,6 +173,72 @@ const app = new Vue({
                       status: 'received'
                   }
               ],
+          },
+          {
+              name: 'Davide',
+              avatar: '_8',
+              visible: true,
+              messages: [
+                  {
+                      date: '10/01/2020 15:30:55',
+                      message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                      status: 'received'
+                  },
+                  {
+                      date: '10/01/2020 15:50:00',
+                      message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                      status: 'sent'
+                  },
+                  {
+                      date: '10/01/2020 15:51:00',
+                      message: 'OK!!',
+                      status: 'received'
+                  }
+              ],
+          },
+          {
+              name: 'Davide',
+              avatar: '_8',
+              visible: true,
+              messages: [
+                  {
+                      date: '10/01/2020 15:30:55',
+                      message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                      status: 'received'
+                  },
+                  {
+                      date: '10/01/2020 15:50:00',
+                      message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                      status: 'sent'
+                  },
+                  {
+                      date: '10/01/2020 15:51:00',
+                      message: 'OK!!',
+                      status: 'received'
+                  }
+              ],
+          },
+          {
+              name: 'Davide',
+              avatar: '_8',
+              visible: true,
+              messages: [
+                  {
+                      date: '10/01/2020 15:30:55',
+                      message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                      status: 'received'
+                  },
+                  {
+                      date: '10/01/2020 15:50:00',
+                      message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                      status: 'sent'
+                  },
+                  {
+                      date: '10/01/2020 15:51:00',
+                      message: 'OK!!',
+                      status: 'received'
+                  }
+              ],
           }
     ],
 
